e93dd62dacbd6563b292b31facaf8daa7cf412e1
Mike <yum650350@gmail.com>
2025-06-18 14:00:16 +0800

    chore: init project

---
+++ packages/handwriting-accelerate-canvas/src/index.ts
@@ -0,0 +1,9 @@
+export const packageName = 'handwriting-accelerate-canvas';
+
+export class HandwritingAccelerateCanvas {
+  _canvas: HTMLCanvasElement;
+
+  constructor() {
+    this._canvas = document.createElement('canvas');
+  }
+}
